424287fab0233a0edea532676e01f6a4aca9c54f
Kumamon <Hallygirl@mail.ru>
2020-10-30 11:09:05 +0300

    add: fetch requests from pubmed

--- src/pages/Sources.js
+++ src/pages/Sources.js
@@ -2,6 +2,43 @@ import React from 'react'
 
 export const Sources = () => {
     return (
-        <h2>Sources</h2>
+        <div className="grid_container">
+            <div className="item">item</div>
+            <div className="item">item</div>
+            <div className="item">item</div>
+            <div className="item">item</div>
+            <div className="item">item</div>
+            <div className="item">item</div>
+            <div className="item">item</div>
+            <div className="item">item</div>
+            <div className="item">item</div>
+            <div className="item">item</div>
+            <div className="item">item</div>
+            <div className="item">item</div>
+
+            <div className="item_max">
+                <div className="grid_3">
+                    <div className="item">item</div>
+                    <div className="item">item</div>
+                    <div className="item">item</div>
+                    <div className="item">item</div>
+                    <div className="item">item</div>
+                    <div className="item">item</div>
+                    <div className="item">item</div>
+                    <div className="item">item</div>
+                    <div className="item">item</div>
+                    <div className="item">item</div>
+                    <div className="item">item</div>
+                    <div className="item">item</div>
+                </div>
+            </div>
+
+            <div className="grid">
+                <div className="item">item</div>
+                <div className="item">item</div>
+                <div className="item">item</div>
+                <div className="item">item</div>
+            </div>
+        </div>
     )
 }
