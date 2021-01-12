df1b37c4b91b0b95161c6987ec8fe69fa5dc02ce
Kumamon <Hallygirl@mail.ru>
2021-01-12 10:07:33 +0300

    add: thoughts

--- src/docs/thoughts.js
+++ src/docs/thoughts.js
@@ -2,9 +2,30 @@ export const thoughts = [
     'I definitely think that people these days are eating a lot more sugars and sweets than they were in\n' +
     '                               past generations, simply because of the availability of sugar. In each local grocery store tou could find\n' +
     '                            almost two or more aisles dedicated to just these types of food products.',
+
     'In the past people ate such types of foods maybe once a week. Nowadays - several times a day. Do\n' +
     '                                you find yourself eating a chocolate bar three times a week?',
+
     'Medical reports are now well-established that eating a lot of refined sugars leads to health problems,\n' +
     '                                such as type 1 or type 2 diabetes, cardiovascular disease and much more. ',
+
     'Have you been paying an attention to what you\'re eating?',
+
+    "Ориентироваться на чувство голода, чтобы начать есть, и чувство умеренногонасыщения, чтобы закончить",
+
+    "Random double-blind studies and meta-analyses - are whta you look for when checking the efficacy of a product",
+
+    'Отсутствие внятной пищевой политики при огромной доступности необъективной и пугающей информации о еде - это боль...',
+
+    "Удовольствие от хорошей еды люди оценивают выше, чем удовольствие от спорта и хобби, чтения или развлечения. Выше " +
+    "ценится только время, потраченное на отдых, семью или секс",
+
+    "Не все сладкие вкусы одинаковы. Скорость и продолжительность воздействия на рецепторы разные",
+
+    "Пик сладости сахара - через 4 сек, а через 10минут ощущения сладости исчезают",
+
+    "Вегетативная нервная система: симпатический отдел (в стрессе, вы спасаетесь от врагов), парасимпатический отдел " +
+    "(вы расслаблены, жкт может отдохнуть)",
+
+
 ];
